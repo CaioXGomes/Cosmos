6e904b291018a4ba159259c4044500d5e7ee2541
Edu-Azevedo <125192442+Edu-Azevedo@users.noreply.github.com>
2023-05-23 19:08:21 -0300

    Add files via upload

--- Cosmos/js/app.js
+++ Cosmos/js/app.js
@@ -19,29 +19,3 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('bx', 'menu-mobile')
-
-// SCROLL SMOOTH -- PRA DESCER NAS SEÇÕES
-
-const menuLinks  = document.querySelectorAll(".navbar-menu a");
-
-function getDistanceFromTheTop(element){
-    const id = element.getAttribute("href");
-    return document.querySelector(id).offsetTop;
-}
-
-function nativeScroll(distanceFromTheTop){
-    window.scroll({
-        top: distanceFromTheTop,
-        behavior: "smooth",
-    });
-}
-
-function scrollToSection(event){
-    event.preventDefault();
-    const distanceFromTheTop = getDistanceFromTheTop(event.target);
-    nativeScroll(distanceFromTheTop);
-}
-
-menuLinks.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-});
